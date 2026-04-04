78795dbfd92cf5ccbca04a8e16113b443f663e8c
ggbrent25-hub <ggbrent25@gmail.com>
2026-04-04 14:48:27 -0400

    Create initial App component with welcome message

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem' }}>
+      <h1>Sharing Joyful Journeys</h1>
+      <p>Welcome! Your app content will appear here.</p>
+    </div>
+  )
+}
